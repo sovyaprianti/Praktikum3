e660ab768bf6cdee025b5dba37eba4c6ebb03f4b
sovyaprianti <sovyaprianti44@gmail.com>
2024-10-22 07:32:02 +0700

    Add files via upload

---
+++ Latihan svy1.py
@@ -0,0 +1,16 @@
+# penggunaan end
+print('A', end='')
+print('B', end='')
+print('C', end='')
+print()
+print('X')
+print('Y')
+print('Z')
+
+# penggunaan separator
+w, x, y, z = 10, 15, 20, 25
+print(w, x, y, z)
+print(w, x, y, z, sep=',')
+print(w, x, y, z, sep='')
+print(w, x, y, z, sep=':')
+print(w, x, y, z, sep='-----')
